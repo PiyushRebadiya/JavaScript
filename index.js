d6787cdedde2ac1495f3ee539cf584185d685f50
piyushrebadiya <piyushrebadiya123@gmail.com>
2021-11-13 10:07:07 +0530

    js sort question practice

---
+++ index.js
@@ -0,0 +1,120 @@
+console.log("Helooooooooooooo");
+
+async function Handler() {
+    //1
+    // console.log(name);
+    // console.log(age);
+    // var name = "Piyush";
+    // let age = 22;
+
+
+    //2
+    // for (let i = 1; i <= 5; i++) {
+    //     setTimeout(() => {
+    //         console.log("i+++++",i);
+    //     }, i * 1000);
+        
+    // }
+
+    //3
+    // let temp = {
+    //     a: 10,
+    //     b: 20,
+    //     sum(){
+    //       return  this.a * this.b
+    //     },
+    //     multi: () => {
+    //         return this.a * this.b
+    //     }
+    // }
+    // console.log(temp.sum());
+    // console.log(temp.multi());
+
+    //4
+    // console.log(+true);
+    // console.log(!abc);
+
+    //5
+    // const bird = {
+    //     size: "small"
+    // }
+    // const mouse = {
+    //     name: "Mickey",
+    //     small: true,
+    // }
+    // console.log(mouse.bird.size);
+    // console.log(mouse[bird.size]);
+    // console.log(mouse[bird["size"]]);
+
+    //6
+    // let c = {greeting: "Hey!"};
+    // let d;
+    // d = c;
+    // d.greeting = "Heloooooo";
+    // console.log(d);
+    // console.log(c);
+
+    //7
+    // let a = 3;
+    // let b = new Number(3);
+    // console.log(a == b);
+    // console.log(a === b);
+
+    //8
+    // let greeting;
+    // greetign = {};
+    // console.log(greetign);
+    // console.log(greeting);
+
+    //9
+    // console.log(10 + "2");
+
+    //10
+    // console.log(typeof []);
+
+    //11
+    // let x = 0;
+    // console.log(x++);
+
+    //12
+    // console.log(eval("10*10+5"));
+
+    //14
+    // var num = 8;
+    // var num = 10;
+    // console.log(num);
+
+    //15
+    // const obj = {
+    //     name: "Piyush",
+    //     name: "Raj"
+    // }
+    // console.log(obj);
+
+    //16
+    // for (let i = 1; i < 5; i++) {
+    //    if(i === 3) continue;
+    //    console.log("i++++",i);
+    // }
+
+    //17
+    // console.log(typeof typeof 1);
+   
+    //18
+    // console.log(true == 1);
+
+    //19
+    let x = [..."Piyush"]
+    console.log(x);
+}
+
+
+Handler()
+// const printNumbersForEvery2Sec = (n)=>{
+//     for (let i = 1; i <= n; i++) {
+//         setTimeout( () =>{
+//           console.log(i)
+//         }, i * 2000)
+//       }
+//   }
+//   printNumbersForEvery2Sec(5);
